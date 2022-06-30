fc4b5ff16a35d41c9c7ebbd0f5265c11d0b42b1f
reiji-dat <reiji.dat@gmail.com>
2022-06-30 13:25:43 +0900

    kannsei

--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,10 +12,38 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
+        int vx = -5;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label2.Left = fpos.X - label2.Width / 2;
+            label2.Top = fpos.Y - label2.Height / 2;
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left < 0)
+            {
+                vx = -vx;
+            }
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -vx;
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -vy;
+            }
+        }
     }
 }
